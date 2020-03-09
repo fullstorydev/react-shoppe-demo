de94cb3f6d103098ad141e1a13976aec3d088d4c
Patrick Brandt <patrick@fullstory.com>
2020-03-08 23:00:30 -0400

    adding React router and placeholder components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import Products from './Products';
+import Checkout from './Checkout';
+import Cart from './Cart';
+import ThankYou from './ThankYou';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>More to come</div>
+    <div>
+      <Switch>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+        <Route path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route path='/thankyou'>
+          <ThankYou />
+        </Route>
+        <Route path='/'>
+          <Products />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
