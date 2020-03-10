e96e8147b495bad4c39e5e1a2b17e8e68e61cf94
Patrick Brandt <patrick@fullstory.com>
2020-03-10 14:04:06 -0400

    updating page title and nav title

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navigation = () => {
   return (
     <Navbar bg='dark' expand='lg' fixed='top'>
-      <Navbar.Brand><Link to='/' className='header-link'>Reactshoppe</Link></Navbar.Brand>
+      <Navbar.Brand><Link to='/' className='header-link'>The Fruit Shoppe</Link></Navbar.Brand>
       <Nav className='mr-auto'></Nav>
       <Nav>
       <Link to='/cart'>Cart</Link>
