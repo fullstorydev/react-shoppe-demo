be21406dce0508acda200b6da440f0c68067973c
Tim Chin <timjchin@gmail.com>
2020-03-13 15:01:47 -0400

    forgot to save!

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,10 @@
 import * as CartActions from './cart';
 import { ActionType } from '../types/helpers';
+import { Dispatch } from 'redux';
 
 export const Actions = {
   ...CartActions,
 }
 
 export type AppActions = ActionType<typeof Actions>;
+export type AppDispatch = Dispatch<AppActions>;
