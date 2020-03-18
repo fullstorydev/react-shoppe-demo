bb9de3e29f6060491c011b723223fd4267a6882d
Patrick Brandt <patrick@fullstory.com>
2020-03-18 07:42:25 -0400

    refactoring cart type into an enum

--- src/actions/cart.ts
+++ src/actions/cart.ts
@@ -1,6 +1,6 @@
 import { Dispatch } from 'redux';
 import { Product } from '../types/product';
-import { ADD_TO_CART } from '../types/cart';
+import { CartTypes } from '../types/cart';
 import { createAction } from '../types/helpers';
 
 export type CartDispatch = Dispatch<CartActionTypes>;
@@ -13,7 +13,7 @@ export const addToCart = (product: Product)  => {
   // NOTE: this will be removed in the next PR
   console.log(`product added: ${JSON.stringify(product)}`);
   return createAction({
-    type: ADD_TO_CART,
+    type: CartTypes.AddToCart,
     product,
   });
 };
