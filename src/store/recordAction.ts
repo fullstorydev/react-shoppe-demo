b51273124d2f21d43f8246eae18bad45287413e1
Patrick Brandt <patrick@fullstory.com>
2020-04-03 14:09:32 -0400

    removing thunk action recording from action recordr middleware

--- src/store/recordAction.ts
+++ src/store/recordAction.ts
@@ -4,8 +4,6 @@ import { Middleware } from 'redux';
 const recordAction: Middleware = () => next => action => {
   if (typeof action !== 'function') {
     FullStory.log(action.type);
-  } else if (typeof action === 'function') {
-    FullStory.log('Action is a thunk');
   }
   return next(action);
 };
